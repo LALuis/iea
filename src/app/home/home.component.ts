fb592a1c74be70a7e613bd43868de1d0480a88c5
Luis Lopez <lopezalvarez.ld@gmail.com>
2018-12-05 23:58:30 -0300

    Galeria y spinner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { BaseService } from './base.service';
 import { Router } from '@angular/router';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-home',
@@ -11,43 +12,45 @@ import { Router } from '@angular/router';
 
 
 export class HomeComponent implements OnInit {
-
+  public loading : boolean = false;
   public decode = "data:image/png;base64,";
   public tipoImagen = {
-      portada:null,
-      nosotros:null,
-      galeriaInstagram:null
-    };
+    portada: null,
+    nosotros: null,
+    galeriaInstagram: null,
+    imagenes:null
+  };
   public dirNum: number = 0;
   public perfNum: number = 0;
+  public galeriaNum: number = 50;
   public perfiles: any = [
     {
-      titulo:'Tech',
-      tareas:'Administrar redes, de la creación y el mantenimiento de nuestra web y la preparación del hadware/software para los talleres.',
-      lider:'Agustín Casenave',
-      imagen:'tech',
-      icon:'fa-cogs'
+      titulo: 'Tech',
+      tareas: 'Administrar redes, de la creación y el mantenimiento de nuestra web y la preparación del hadware/software para los talleres.',
+      lider: 'Agustín Casenave',
+      imagen: 'tech',
+      icon: 'fa-cogs'
     },
     {
-      titulo:'Contenido',
-      tareas:'Crear material didáctico y recreativo para la realización de los talleres. Tanto para los niños como para los voluntarios que asisten a dichos talleres.',
-      lider:'Gimena Alamón',
-      imagen:'contenido',
-      icon:'fa-suitcase'
+      titulo: 'Contenido',
+      tareas: 'Crear material didáctico y recreativo para la realización de los talleres. Tanto para los niños como para los voluntarios que asisten a dichos talleres.',
+      lider: 'Gimena Alamón',
+      imagen: 'contenido',
+      icon: 'fa-suitcase'
     },
     {
-      titulo:'Gestión',
-      tareas:'La organización de IEA, administrar los recursos (humanos y materiales), logística de las actividades sociales y difusión empresarial.',
-      lider:'Fernanda Sesto',
-      imagen:'gestion',
-      icon:'fa-line-chart'
+      titulo: 'Gestión',
+      tareas: 'La organización de IEA, administrar los recursos (humanos y materiales), logística de las actividades sociales y difusión empresarial.',
+      lider: 'Fernanda Sesto',
+      imagen: 'gestion',
+      icon: 'fa-line-chart'
     },
     {
-      titulo:'Voluntariado',
-      tareas:'Planificar estrategias para captar mas voluntarios, realizar campañas de recolección de materiales y actividades para fomentar el trabajo en equipo.',
-      lider:'Nicolás Colombo.',
-      imagen:'voluntariado',
-      icon:'fa-thumbs-up'
+      titulo: 'Voluntariado',
+      tareas: 'Planificar estrategias para captar mas voluntarios, realizar campañas de recolección de materiales y actividades para fomentar el trabajo en equipo.',
+      lider: 'Nicolás Colombo.',
+      imagen: 'voluntariado',
+      icon: 'fa-thumbs-up'
     }
   ];
 
@@ -121,6 +124,7 @@ export class HomeComponent implements OnInit {
   }
 
   getAllImages() {
+    this.loading = true;
     this.baseService.getAllImages().subscribe(res => {
       this.tipoImagen['portada'] = null;
       this.tipoImagen['nosotros'] = null;
@@ -130,24 +134,33 @@ export class HomeComponent implements OnInit {
         let data = element.data();
         switch (data.nombre) {
           case 'portada': {
-            let obj = Object.assign({}, data)
+            let obj = Object.assign({}, data);
             this.tipoImagen['portada'] = obj;
             break;
           }
           case 'nosotros': {
-            let obj = Object.assign({}, data)
+            let obj = Object.assign({}, data);
             this.tipoImagen['nosotros'] = obj;
             break
           }
           case 'galeriaInstagram': {
-            let obj = Object.assign({}, data)
+            let obj = Object.assign({}, data);
             this.tipoImagen['galeriaInstagram'] = obj;
             break;
           }
+          default:{
+            let obj = Object.assign({}, data);
+            this.tipoImagen['imagenes']
+          }
+
         }
         console.log(this.tipoImagen);
       });
-    })
+      this.loading = false;
+    },
+    error => {
+      this.loading = false;
+    });
   }
 
   navegar(ruta) {
@@ -155,21 +168,41 @@ export class HomeComponent implements OnInit {
     return false;
   }
 
-  cambioDir(movimiento){
-    if(movimiento === 'SIG'){
-      if(this.dirNum != this.directorio.length -1){
+  cambioDir(movimiento) {
+    if (movimiento === 'SIG') {
+      if (this.dirNum != this.directorio.length - 1) {
         this.dirNum++;
       }
-      else{
+      else {
         this.dirNum = 0;
       }
     }
-    else{
-      if(this.dirNum != 0){
+    else {
+      if (this.dirNum != 0) {
         this.dirNum--;
       }
-      else{
-        this.dirNum = this.directorio.length -1;
+      else {
+        this.dirNum = this.directorio.length - 1;
+      }
+    }
+    return false;
+  }
+
+  cambioGaleria(movimiento){
+    if (movimiento === 'SIG') {
+      if (this.galeriaNum != 56) {
+        this.galeriaNum++;
+      }
+      else {
+        this.galeriaNum = 1;
+      }
+    }
+    else {
+      if (this.galeriaNum != 1) {
+        this.galeriaNum--;
+      }
+      else {
+        this.galeriaNum = 56;
       }
     }
     return false;
